f80d1ed50b028de951ff94c366613e82e78c5ecd
Xethyr <monteiv@me.com>
2024-06-20 14:14:19 -0500

    Completed exercise 2.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let fullString = string;
+    if (num === 0) {
+        return '';
+    }
+    if (num < 0) {
+        return 'ERROR';
+    }
+    for (let i = 0; i < num - 1; i++) {
+        fullString += string;
+    }
+    return fullString;
 };
 
 // Do not edit below this line
